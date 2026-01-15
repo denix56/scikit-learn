263b9b804eff5680ad852610b0a735d01e074781
denyssenkin <denisx9.0c@gmail.com>
2026-01-15 18:59:19 +0100

    Fix merge

--- sklearn/tree/_tree.pyx
+++ sklearn/tree/_tree.pyx
@@ -325,7 +325,7 @@ cdef class DepthFirstTreeBuilder(TreeBuilder):
         _init_parent_record(&parent_record)
 
         cdef float64_t[:] impurity = tree.impurity
-        cdef unordered_map[intp_t, vector[vector[float64_t]]]& value_samples = tree.value_samples
+        cdef unordered_map[intp_t, vector[vector[float64_t]]]* value_samples = &tree.value_samples
 
         with nogil:
             if not first:
@@ -484,7 +484,7 @@ cdef class DepthFirstTreeBuilder(TreeBuilder):
                     })
                 elif store_leaf_values and is_leaf:
                     # copy leaf values to leaf_values array
-                    splitter.node_samples(value_samples[node_id])
+                    splitter.node_samples((*value_samples)[node_id])
 
                 if depth > max_depth_seen:
                     max_depth_seen = depth
@@ -609,7 +609,7 @@ cdef class DepthFirstTreeBuilder(TreeBuilder):
                     })
                 elif store_leaf_values and is_leaf:
                     # copy leaf values to leaf_values array
-                    splitter.node_samples(value_samples[node_id])
+                    splitter.node_samples((*value_samples)[node_id])
 
                 if depth > max_depth_seen:
                     max_depth_seen = depth
