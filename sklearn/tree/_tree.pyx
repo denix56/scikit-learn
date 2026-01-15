58972fe41222f9d04b7a3e324bcb64fd8f87bd42
denyssenkin <denisx9.0c@gmail.com>
2026-01-15 19:01:12 +0100

    Fix merge

--- sklearn/tree/_tree.pyx
+++ sklearn/tree/_tree.pyx
@@ -484,7 +484,7 @@ cdef class DepthFirstTreeBuilder(TreeBuilder):
                     })
                 elif store_leaf_values and is_leaf:
                     # copy leaf values to leaf_values array
-                    splitter.node_samples((*value_samples)[node_id])
+                    splitter.node_samples(deref(value_samples)[node_id])
 
                 if depth > max_depth_seen:
                     max_depth_seen = depth
@@ -609,7 +609,7 @@ cdef class DepthFirstTreeBuilder(TreeBuilder):
                     })
                 elif store_leaf_values and is_leaf:
                     # copy leaf values to leaf_values array
-                    splitter.node_samples((*value_samples)[node_id])
+                    splitter.node_samples(deref(value_samples)[node_id])
 
                 if depth > max_depth_seen:
                     max_depth_seen = depth
